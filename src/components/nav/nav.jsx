89ac8cff069421f129aaec8240de42a40b8b8079
CrazyBucket <1475743203@qq.com>
2022-09-18 20:40:03 +0800

    :sparkles: 增加搜索组件，尝试了父子通信

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -2,29 +2,41 @@ import './nav.css'
 import React from 'react'
 import img1 from '../../../public/img/readhub.png'
 import img2 from '../../../public/img/wuma.png'
-import { NavLink, Route, Routes } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { useState } from 'react';
+import Search from '../search/search.jsx'
 
 export const nav = () => {
-  return (
-    <div className="nav">
-        <div className="container">
-            <div className="left1">
-                <div className="logo_box">
-                    <img src={img2} height="15px"/>
-                    <div className='separate'></div>
-                    <img src={img1} width="80px" />
+    const [mask,setMask] = useState(false)
+    const close = () => {
+        setMask(!mask)
+    }
+    return (
+        <div>
+            <Search mask={mask} close={close}/>
+            <div className="nav">
+                <div className="container">
+                    <div className="left1">
+                        <div className="logo_box">
+                            <img src={img2} height="15px"/>
+                            <div className='separate'></div>
+                            <img src={img1} width="80px" />
+                        </div>
+                        <NavLink to='/topic' className = {({ isActive }) => isActive ? 'selected' : 'header'}><div>热门话题</div></NavLink>
+                        <NavLink to='/daily' className = {({ isActive }) => isActive ? 'selected' : 'header'}><div>每日早报</div></NavLink>
+                        <NavLink to='/news' className = {({ isActive }) => isActive ? 'selected' : 'header'}><div>科技动态</div></NavLink>
+                        <NavLink to='/tech' className = {({ isActive }) => isActive ? 'selected' : 'header'}><div>技术资讯</div></NavLink>
+                    </div>
+                    <div className="right1">
+                        <div className="search_box"onClick={() => {
+                            setMask(!mask)
+                        }}>
+                        <div className="searchImg"></div></div>
+                        <div className="login"><a href="/login">登陆</a></div>
+                    </div>
                 </div>
-                <NavLink to='/topic' className = {({ isActive }) => isActive ? 'selected' : 'header'}><div>热门话题</div></NavLink>
-                <NavLink to='/daily' className = {({ isActive }) => isActive ? 'selected' : 'header'}><div>每日早报</div></NavLink>
-                <NavLink to='/news' className = {({ isActive }) => isActive ? 'selected' : 'header'}><div>科技动态</div></NavLink>
-                <NavLink to='/tech' className = {({ isActive }) => isActive ? 'selected' : 'header'}><div>技术资讯</div></NavLink>
-            </div>
-            <div className="right1">
-                <div className="search_box"><div className="searchImg"></div></div>
-                <div className="login"><a href="/login">登陆</a></div>
-            </div>
+            </div> 
         </div>
-    </div>
-  )
+    )
 }
 export default nav
---
+++ src/components/search/search.jsx
@@ -0,0 +1,28 @@
+import './search.css'
+import React from 'react'
+import img3 from '../../../public/img/logo.png'
+
+export const search = (props) => {
+    const { mask,close } = props
+    return (
+            <div>
+                {
+                    mask?(
+                        <div className="search">
+                            <div className="search_container">
+                                <div className="old_logo"><img src={img3} height="36px"/></div>
+                                <div className='input_box'>
+                                    <div className="search_icon"><div className="searchImg"></div></div>
+                                    <input className="search_input" placeholder='搜索' autoFocus></input>
+                                </div>
+                            </div>
+                            <div className={mask?"mask":""} onClick={() => {
+                                close();
+                            }}></div>
+                        </div>
+                    ):null
+                }
+            </div>
+    )
+}
+export default search
